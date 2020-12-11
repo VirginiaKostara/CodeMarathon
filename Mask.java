b7f2b04d46df7d27e244334e9a96cb034a24d41d
MarthaBarlogianni <72819086+MarthaBarlogianni@users.noreply.github.com>
2020-12-11 18:28:20 +0100

    changes in initialization method

--- Mask.java
+++ Mask.java
@@ -31,7 +31,7 @@ public class Mask {
 	}    //Μέθοδος αλλαγής της κατάστασης εαν ο εργαζόμενος του αντικειμένου πέρασε σε τηλεργασία λόγω της μάσκας
 
 	public static void initialization() {
-		for (int i=0 ; i<49 ; i++) {
+		for (int i=0 ; i<50 ; i++) {
 			new Mask(0,false);
 		}
 	}
