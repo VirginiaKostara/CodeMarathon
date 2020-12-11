da174dc38c9d10cdf6bb0749fc2c7539915899f5
ElpidaChristodoulea <72852799+ElpidaChristodoulea@users.noreply.github.com>
2020-12-11 16:36:32 +0200

    changes in printCasesnow and printCases

--- CovidCases.java
+++ CovidCases.java
@@ -1,4 +1,4 @@
-
+package codemarathon;
 public class CovidCases {
     static int count=0;
 	public static CovidCases[]casesnow=new CovidCases[3];
@@ -7,22 +7,22 @@ public class CovidCases {
 	private String name;
 	private String surname;
     private int daysleft;
-	
+
 	public CovidCases (int idcases) {
 		casesnow[count]=this;
 		count++;
 		this.idcases = idcases;
 		this.name = Employee.employees[idcases-1].getName();
 		this.surname = Employee.employees[idcases-1].getSurname();;
-		this.daysleft = 14 - quarantine_days[idcases-1];
+		this.daysleft = 12; //To testCovid-19 κανει 2 ημερες να βγει, επομένως αντίστροφη μέτρηση απο την 12η ημέρα
 	}
-	
-	
+
+
 	@Override
 	public String toString() {
 		return "casesnow [idcases=" + idcases + ", name=" + name + ", surname=" + surname + ", daysleft=" + daysleft + "]" ;
 	}
-	
+
 	public void setDaysleft(int daysleft) {
 		this.daysleft = daysleft;
 	}
@@ -32,49 +32,70 @@ public class CovidCases {
 		cases[id-1]=Employee.employees[id-1];
 		boolean flag=false;
 		int i=0;
-		while(flag==false && i<casesnow.length ) {	
+		while(flag==false && i<casesnow.length ) {
 			    if(casesnow[i]==null) {
 			    	 new CovidCases(id);
 			    	 flag=true;
 			    } else {
 			    	i++;
+			}
+			    if(i==casesnow.length) {
+			    	System.out.print("ΥΠΟΧΡΕΩΤΙΚΟ ΚΛΕΙΣΙΜΟ ΕΤΑΙΡΕΙΑΣ!");
 			    }
-				
-			} 
-		if(i=casesnow.length) {
-			System.out.print("ΥΠΟΧΡΕΩΤΙΚΟ ΚΛΕΙΣΙΜΟ ΕΤΑΙΡΕΙΑΣ!");
-			 
 		}
 	}
-		
 
-	
+
+
 	public static void printCases() {
+		int count = 0;
 		for(int i=0 ; i<cases.length ; i++) {
 			if(cases[i]!=null){
-				cases[i].toString();
-			} 
+				count++;
+			}
+		}
+		if (count == 0 ) {
+			System.out.println("Δεν έχει υπάρξει κρούσμα ως τώρα");
+		} else {
+			System.out.println("Οι εργαζόμενοι που έχουν υπάρξει κρούσματα ή είναι αυτή τη στιγμή είναι οι:");
+			for(int i=0 ; i<cases.length ; i++) {
+				if(cases[i]!=null){
+					cases[i].toString();
+				}
+			}
 		}
 	}
-	
+
 	public static void deleteCase() { // Κάθε καινούρια μέρα που αλλάζει από την main, καλούμε την deleteCase
 		for(int i=0 ; i<casesnow.length; i++) {
 			if(casesnow[i].daysleft==0) {
 				casesnow[i]=null;
-				
+				count--;
+
 			}
 		}
 	}
-	
+
 	public static void printCasesnow() {
+		int count = 0;
 		for(int i=0 ; i<casesnow.length ; i++) {
 			if(casesnow[i]!=null){
 				System.out.println(casesnow[i]);
+				count++;
+			}
+		}
+		if (count == 0) {
+			System.out.println("Δεν υπάρχει κανένα ενεργό κρούσμα");
+		}else {
+			System.out.println("Οι εργαζόμενοι που είναι ενεργά κρούσματα είναι οι:");
+			for(int i=0 ; i<casesnow.length ; i++) {
+				if(casesnow[i]!=null){
+					System.out.println(casesnow[i]);
+				}
 			}
 		}
 	}
-	
-    
+
+
 
 }
-	
