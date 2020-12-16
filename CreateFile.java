b7c481d1e07a138972a0552f8f67f382d80a4369
AngelikiBoura <73474686+AngelikiBoura@users.noreply.github.com>
2020-12-16 16:07:32 +0200

    2nd Comission for CreateFile with comments

--- CreateFile.java
+++ CreateFile.java
@@ -4,14 +4,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class CreateFile {
-
-	public void Create() {
+	// στατική μέθοδος για την αποθήκευση των αλλαγών πριν κάθε τερματισμό της εφαρμογής
+	public void saveData() {
 		FileWriter writer;
 		try {
-			writer = new FileWriter(new File("/home/natasa/eclipse-workspace/codemarathon/src/codemarathon/employee2.txt"));
-
+			//Το αρχείο αυτό την πρώτη μέρα δημιουργείται και τις επόμενες ανανεώνεται
+			writer = new FileWriter(new File("employees2.txt"));
+			//Αποθηκεύουμε την ημέρα λειτουργίας της εφαρμογής
 			writer.write("CURRENT_DAY " + Main.day + "\n");
-
+			//Αποθηκέυουμε για κάθε υπάλληλο το id και τα στοιχεία του που μπορούν να  μεταβληθούν
 			for (int i=0; i<50; i++) {
 
 				writer.write("{" + "\n" + "ID " + Telework.teleworkers[i].getId() + "\n"
@@ -20,10 +21,11 @@ public class CreateFile {
 						+ "QUARANTINE_RESPONSIBLE " + Telework.teleworkers[i].getQuarantine_responsible() + "\n"
 						+ "TIMESNOMASK " + Mask.nomask[i].getTimes() + "\n"
 						+ "DONE_TELEWORK " + Mask.nomask[i].getDoneTelework() + "\n"
+						+ "HAD_COVID " + Employee.employees[i].getHad_covid() + "\n"
 						+ "}" +"\n" );
 			}
 			writer.close();
-
+			//Εξαίρεση σε περίπτωση που υπάρξει πρόβλημα με την συγγραφή του αρχείου
 		} catch (IOException e) {
 			System.err.println("Error writing file.");
 		}
