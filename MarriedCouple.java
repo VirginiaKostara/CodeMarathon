eabf5a7279df8eaec4eff8bce648d994406a180a
Virginia Kostara <72817607+VirginiaKostara@users.noreply.github.com>
2020-12-22 19:10:15 +0200

    MarriedCouple.java

---
+++ MarriedCouple.java
@@ -0,0 +1,70 @@
+package CodeMarathon;
+
+public class MarriedCouple<E1, E2> { 
+  private E1 e1;
+  private E2 e2;
+    
+  public MarriedCouple(E1 e1, E2 e2) {
+    this.e1 = e1;
+    this.e2 = e2;
+  }
+   
+  static boolean f;
+  /** 
+   * Make sure that two spouses either work at the company or at home.
+   */
+  
+  public void compareobjects() {
+    int id1;
+    int id2;
+    if (this.e2 instanceof Telework && this.e1 instanceof Telework) {
+      for (int i = 0; i < Telework.teleworkers.length; i++) {
+        f = true;
+        if (e2.equals(Telework.teleworkers[i])) {
+          f = false;
+          for (int z = 0; z < Telework.teleworkers.length; z++) {
+            if (e1.equals(Telework.teleworkers[i])) {
+              if (((Telework) e1).getWorkStatus() != Telework.Status.NORMAL
+                                    && ((Telework) e2).getWorkStatus() == Telework.Status.NORMAL) {
+                id1 = ((Telework) e1).getId();
+                id2 = ((Telework) e2).getId();
+                Telework.teleworkers[id2 - 1].setWorkStatus(Telework.Status.UNCONFIRMEDCASE2);
+                Telework.teleworkers[id2 - 1].setQuarantine_responsible(
+                     Telework.teleworkers[id1 - 1].getQuarantine_responsible());
+                Telework.teleworkers[id2 - 1].setQuarantine_days(1);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @Override
+    public boolean equals(Object obj) {
+    if (obj == null) {
+      return false;
+    }
+    if (obj == this) {
+      return true;
+    }
+    Telework telework = (Telework) obj;
+    if (f) {
+      return ((Telework) e2).getQuarantine_responsible() == telework.getQuarantine_responsible()
+                    && ((Telework) e2).getQuarantine_days() == telework.getQuarantine_days()
+                    && ((Telework) e2).getWorkStatus() == telework.getWorkStatus()
+                    && ((Telework) e2).getName() == telework.getName()
+                    && ((Telework) e2).getSurname() == telework.getSurname()
+                    && ((Telework) e2).getUnit() == telework.getUnit()
+                    && ((Telework) e2).getTransportation() == telework.getTransportation();
+    } else {
+      return ((Telework) e1).getQuarantine_responsible() == telework.getQuarantine_responsible()
+                    && ((Telework) e1).getQuarantine_days() == telework.getQuarantine_days()
+                    && ((Telework) e1).getWorkStatus() == telework.getWorkStatus()
+                    && ((Telework) e1).getName() == telework.getName()
+                    && ((Telework) e1).getSurname() == telework.getSurname()
+                    && ((Telework) e1).getUnit() == telework.getUnit()
+                    && ((Telework) e1).getTransportation() == telework.getTransportation();
+    }
+  }
+}
