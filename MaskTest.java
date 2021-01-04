74fd399988f5f2e3c311e4ee60643cd70b255467
MarthaBarlogianni <72819086+MarthaBarlogianni@users.noreply.github.com>
2021-01-04 19:17:45 +0200

    checkstyle changes

--- MaskTest.java
+++ MaskTest.java
@@ -5,19 +5,19 @@ import org.junit.jupiter.api.Test;
 
 class MaskTest {
 
-	@Test
-	void testGetTimes() {
-		Mask test = new Mask(3, false);
-		int result = test.getTimes();
-		assertEquals(3, result);
-	}
+  @Test
+    void testGetTimes() {
+    Mask test = new Mask(3, false);
+    int result = test.getTimes();
+    assertEquals(3, result);
+  }
 
-	@Test
-	void testGetDoneTelework() {
-		Mask test = new Mask(3, false);
-		boolean result = test.getDoneTelework();
-		assertEquals(false, result);
-	}
+  @Test
+    void testGetDoneTelework() {
+    Mask test = new Mask(3, false);
+    boolean result = test.getDoneTelework();
+    assertEquals(false, result);
+  }
 
 
 }
