1916273723d46019c9dc04a4e195dde0778c45a5
AngelikiBoura <73474686+AngelikiBoura@users.noreply.github.com>
2021-01-05 20:17:50 +0200

    For saving and updating casetreatment fields

--- CreateFile.java
+++ CreateFile.java
@@ -1,35 +1,48 @@
 package codemarathon;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * A class for saving our data and their changes before every 
+ * time the app shuts down.
+ */
 public class CreateFile {
-	// στατική μέθοδος για την αποθήκευση των αλλαγών πριν κάθε τερματισμό της εφαρμογής
-	public void saveData() {
-		FileWriter writer;
-		try {
-			//Το αρχείο αυτό την πρώτη μέρα δημιουργείται και τις επόμενες ανανεώνεται
-			writer = new FileWriter(new File("/home/natasa/eclipse-workspace/codemarathon/src/codemarathon/employees2.txt"));
-			//Αποθηκεύουμε την ημέρα λειτουργίας της εφαρμογής
-			writer.write("CURRENT_DAY " + Main.day + "\n");
-			//Αποθηκέυουμε για κάθε υπάλληλο το id και τα στοιχεία του που μπορούν να  μεταβληθούν
-			for (int i=0; i<50; i++) {
+  /** This method is being called whenever we terminate the app
+   * in order to save any possible changes that may have occured. 
+   */
 
-				writer.write("{" + "\n" + "ID " + Telework.teleworkers[i].getId() + "\n"
-						+ "STATUS " + Telework.teleworkers[i].getWorkStatus() + "\n"
-						+ "QUARANTINE_DAYS " + Telework.teleworkers[i].getQuarantine_days() + "\n"
-						+ "QUARANTINE_RESPONSIBLE " + Telework.teleworkers[i].getQuarantine_responsible() + "\n"
-						+ "TIMESNOMASK " + Mask.nomask[i].getTimes() + "\n"
-						+ "DONE_TELEWORK " + Mask.nomask[i].getDoneTelework() + "\n"
-						+ "HAD_COVID " + Employee.employees[i].getHad_covid() + "\n"
-						+ "}" +"\n" );
-			}
-			writer.close();
-			//Εξαίρεση σε περίπτωση που υπάρξει πρόβλημα με την συγγραφή του αρχείου
-		} catch (IOException e) {
-			System.err.println("Error writing file.");
-		}
-	}
+  public void saveData() {
+    FileWriter writer;
+    try {
+      //Το αρχείο αυτό την πρώτη μέρα δημιουργείται και τις επόμενες ανανεώνεται
+      writer = new FileWriter(new File("/home/natasa/eclipse-workspace/codemarathon/src/"
+        + "codemarathon/employees2.txt"));
+      //Αποθηκεύουμε την ημέρα λειτουργίας της εφαρμογής
+      writer.write("CURRENT_DAY " + Main.day + "\n");
+      //Αποθηκέυουμε για κάθε υπάλληλο το id και τα στοιχεία του που μπορούν να  μεταβληθούν
+      for (int i = 0; i < 50; i++) {
+        writer.write("{" + "\n" + "ID " + Telework.teleworkers[i].getId() + "\n"
+            + "STATUS " + Telework.teleworkers[i].getWorkStatus() + "\n"
+            + "QUARANTINE_DAYS " + Telework.teleworkers[i].getQuarantine_days() + "\n"
+            + "QUARANTINE_RESPONSIBLE " + Telework.teleworkers[i].getQuarantine_responsible() + "\n"
+            + "TIMESNOMASK " + Mask.nomask[i].getTimes() + "\n"
+            + "DONE_TELEWORK " + Mask.nomask[i].getDoneTelework() + "\n"
+            + "HAD_COVID " + Employee.employees[i].getHad_covid() + "\n"
+            + "TODAY'S_FEVER " + CaseTreatment.casetreatments[i].getFever() + "\n"
+            + "TODAY'S_SYMPTOMS " + CaseTreatment.casetreatments[i].getSymptoms() + "\n"
+            + "TODAY'S_HOSPITAL " + CaseTreatment.casetreatments[i].getHospital() + "\n"
+            + "DAYS_SYMPTOMS " + CaseTreatment.casetreatments[i].getDaysymptoms() + "\n"
+            + "DAYS_inHOSPITAL " + CaseTreatment.casetreatments[i].getDayhospital() + "\n"
+            + "}" + "\n");
+      }
+      writer.close();
+	  //Εξαίρεση σε περίπτωση που υπάρξει πρόβλημα με την συγγραφή του αρχείου
+    } catch (IOException e) {
+      System.err.println("Error writing file.");
+    }
+  }
 
 }
 
--- Readfile.java
+++ Readfile.java
@@ -1,118 +1,152 @@
 package codemarathon;
+
+import codemarathon.Telework.Status;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-import codemarathon.Telework.Status;
 
+/**
+ * A class for creating Employee,Telework objects by using file employees1.txt
+ * and updating the app's memory by using employees2.txt where all the info that
+ * associate with covid and telework are being saved.
+ *
+ */
 public class Readfile {
-	private Scanner x;
-	public void openFile() {
-		try {
+  private Scanner x;
+  
+  /** A method that connects the app with the employees1.txt file.
+   */
+  public void openFile() {
+    try {
+      x = new Scanner(new File("/home/natasa/eclipse-workspace/"
+        + "codemarathon/src/codemarathon/employees1.txt"));
+    } catch (Exception e) {
+      System.out.println("not found file");
+    }
+  }
 
-			x= new Scanner(new File("/home/natasa/eclipse-workspace/codemarathon/src/codemarathon/employees1.txt"));
-		}
-		catch(Exception e) {
-			System.out.println("not found file");
-		}
-	}
-	public  void CreateEmploee() {
-		while(x.hasNext()) {
-			String a= x.next();
-			String b = x.next();
-			String c = x.next();
-			String d = x.next();
-			String e = x.next();
-			String f = x.next();
-			new Telework(a,b,c,d,e,f,Status.NORMAL,-2,0);
-		}
+  /**
+   *This method calls the Telework constructor to generate the Telework objects
+  * as well as the Employee ones.  **/
+  public  void createEmploee() {
+    while (x.hasNext()) {
+      String a = x.next();
+      String b = x.next();
+      String c = x.next();
+      String d = x.next();
+      String e = x.next();
+      String f = x.next();
+      new Telework(a, b, c, d, e, f, Status.NORMAL, -2, 0);
+    }
 
-	}
-	public void closeFile() {
-		x.close();
-	}
+  }
 
-	public void UpdateData() {
+  public void closeFile() {
+    x.close();
+  }
 
-		BufferedReader reader = null;
-		String line, temp;
-		int counter=0;
+  /**
+   * This method updates our data and gives memory to the app
+   * considering every change that has occured
+   * regarding the company's employees and the current day.
+  */
+  public void updateData() {
+    BufferedReader reader = null;
+    String line;
+    String temp;
+    int counter = 0;
+    try {
+      reader = new BufferedReader(new FileReader(new File("/home/natasa/eclipse-workspace/"
+       + "codemarathon/src/codemarathon/employees2.txt")));
+      line = reader.readLine();
+      while (line != null) {
+        if (line.trim().startsWith("CURRENT_DAY")) {
+          Main.day = Integer.parseInt(line.trim().substring(12));
+        }
+        line = reader.readLine();
+        while (line != null) {
+          if (line.trim().equals("{")) {
+            line = reader.readLine();
+            while (!line.trim().equals("}")) {
+              if (line.trim().startsWith("ID")) {
+                counter = Integer.parseInt(line.trim().substring(3)) - 1;
+              } else if (line.trim().startsWith("STATUS")) {
+                temp = line.trim().substring(7);
+                if (temp.equals("NORMAL")) {
+                  Telework.teleworkers[counter].setWorkStatus(Status.NORMAL);
+                } else if (temp.equals("UNCONFIRMEDCASE1")) {
+                  Telework.teleworkers[counter].setWorkStatus(
+                      Status.UNCONFIRMEDCASE1);
+                } else if (temp.equals("UNCONFIRMEDCASE2")) {
+                  Telework.teleworkers[counter].setWorkStatus(
+                      Status.UNCONFIRMEDCASE2);
+                } else if (temp.equals("COVIDCASE")) {
+                  Telework.teleworkers[counter].setWorkStatus(Status.COVIDCASE);
+                }
 
-		try {
+              } else if (line.trim().startsWith("QUARANTINE_DAYS")) {
+                Telework.teleworkers[counter].setQuarantine_days(
+                    Integer.parseInt(line.trim().substring(16)));
+              } else if (line.trim().startsWith("QUARANTINE_RESPONSIBLE")) {
+                Telework.teleworkers[counter].setQuarantine_responsible(
+                    Integer.parseInt(line.trim().substring(23)));
+              } else if (line.trim().startsWith("TIMESNOMASK")) {
+                Mask.nomask[counter].setTimes(
+                    Integer.parseInt(line.trim().substring(12)));
+              } else if (line.trim().startsWith("DONE_TELEWORK")) {
+                temp = line.trim().substring(14);
+                if (temp.equals("true")) {
+                  Mask.nomask[counter].setDoneTelework(true);
+                } else if (temp.equals("false")) {
+                  Mask.nomask[counter].setDoneTelework(false);
+                }
 
-			reader = new BufferedReader(new FileReader(new File("/home/natasa/eclipse-workspace/codemarathon/src/codemarathon/employees2.txt")));
-			line = reader.readLine();
-			while(line!=null) {
-				if(line.trim().startsWith("CURRENT_DAY")) {
-					Main.day = Integer.parseInt(line.trim().substring(12));
-				}
-				line = reader.readLine();
-				while(line!=null) {
+              } else if (line.trim().startsWith("HAD_COVID")) {
+                temp = line.trim().substring(10);
+                if (temp.equals("true")) {
+                  Telework.teleworkers[counter].setHad_covid(true);
+                } else if (temp.equals("false")) {
+                  Telework.teleworkers[counter].setHad_covid(false);
+                }
 
-					if (line.trim().equals("{")) {
-						line = reader.readLine();
-
-						while (!line.trim().equals("}")) {
-
-							if(line.trim().startsWith("ID")) {
-								counter = Integer.parseInt(line.trim().substring(3))- 1;
-
-							} else if(line.trim().startsWith("STATUS")) {
-								temp = line.trim().substring(7);
-
-								if (temp.equals("NORMAL")) {
-									Telework.teleworkers[counter].setWorkStatus(Status.NORMAL);
-								} else if (temp.equals("UNCONFIRMEDCASE1")) {
-									Telework.teleworkers[counter].setWorkStatus(Status.UNCONFIRMEDCASE1);
-								} else if (temp.equals("UNCONFIRMEDCASE2")) {
-									Telework.teleworkers[counter].setWorkStatus(Status.UNCONFIRMEDCASE2);
-								} else if (temp.equals("COVIDCASE")) {
-									Telework.teleworkers[counter].setWorkStatus(Status.COVIDCASE);
-								}
-
-							} else if(line.trim().startsWith("QUARANTINE_DAYS")) {
-								Telework.teleworkers[counter].setQuarantine_days(Integer.parseInt(line.trim().substring(16)));
-
-							} else if(line.trim().startsWith("QUARANTINE_RESPONSIBLE")) {
-								Telework.teleworkers[counter].setQuarantine_responsible(Integer.parseInt(line.trim().substring(23)));
-
-							} else if(line.trim().startsWith("TIMESNOMASK")) {
-								Mask.nomask[counter].setTimes(Integer.parseInt(line.trim().substring(12)));
-
-							} else if(line.trim().startsWith("DONE_TELEWORK")) {
-								temp = line.trim().substring(14);
-
-								if (temp.equals("true")) {
-									Mask.nomask[counter].setDoneTelework(true);
-								} else if (temp.equals("false")) {
-									Mask.nomask[counter].setDoneTelework(false);
-								}
-
-							} else if(line.trim().startsWith("HAD_COVID")) {
-								temp = line.trim().substring(10);
-
-								if (temp.equals("true")) {
-									Telework.teleworkers[counter].setHad_covid(true);
-								} else if (temp.equals("false")) {
-									Telework.teleworkers[counter].setHad_covid(false);
-								}
-
-							}
-							line = reader.readLine();
-						}
-					}
-					line = reader.readLine();
-				}
-				line = reader.readLine();
-			}
-			reader.close();
-
-		} catch (IOException e) {
-			System.err.print("Cannot read file");
-		}
-	}
+              } else if (line.trim().startsWith("TODAY'S_FEVER")) {
+                temp = line.trim().substring(14);
+                CaseTreatment.casetreatments[counter].setFever(
+                    Double.parseDouble(temp));
+              } else if (line.trim().startsWith("TODAY'S_SYMPTOMS")) {
+                temp = line.trim().substring(17);
+                CaseTreatment.casetreatments[counter].setSymptoms(temp);
+              } else if (line.trim().startsWith("TODAY'S_HOSPITAL")) {
+                temp = line.trim().substring(17);
+                if (temp.equals("true")) {
+                  CaseTreatment.casetreatments[counter].setHospital(true);
+                } else if (temp.equals("false")) {
+                  CaseTreatment.casetreatments[counter].setHospital(false);
+                }
+              } else if (line.trim().startsWith("DAYS_SYMPTOMS")) {
+                temp = line.trim().substring(14);
+                CaseTreatment.casetreatments[counter].setDaysymptoms(
+                    Integer.parseInt(temp));
+              } else if (line.trim().startsWith("DAYS_inHOSPITAL")) {
+                temp = line.trim().substring(16);
+                CaseTreatment.casetreatments[counter].setDayhospital(
+                      Integer.parseInt(temp));
+              }
+              line = reader.readLine();
+            }
+          }
+          line = reader.readLine();
+        }
+        line = reader.readLine();
+      }
+      reader.close();
+    } catch (IOException e) {
+      System.err.print("Cannot read file");
+    }
+  }
 }
 
 
