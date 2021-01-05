d543a2954c8344ca72e4457fcf6f6ff6a27c2aac
MarthaBarlogianni <72819086+MarthaBarlogianni@users.noreply.github.com>
2021-01-05 19:08:18 +0200

    tests added

--- MaskTest.java
+++ MaskTest.java
@@ -13,11 +13,27 @@ class MaskTest {
   }
 
   @Test
-    void testGetDoneTelework() {
+   void testGetDoneTelework() {
     Mask test = new Mask(3, false);
     boolean result = test.getDoneTelework();
     assertEquals(false, result);
   }
+
+  @Test
+  void testSetDoneTelework() {
+   Mask test = new Mask(3, false);
+   test.setDoneTelework(true);
+   boolean result = test.getDoneTelework();
+   assertEquals(true, result);
+ }
+
+  @Test
+  void testSetTimes() {
+  Mask test = new Mask(3, false);
+  test.setTimes(8);
+  int result = test.getTimes();
+  assertEquals(8, result);
+}
 
 
 }
