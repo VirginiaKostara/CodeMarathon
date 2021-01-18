4941f36242c164e111db8e17bba60d4b64982341
platobak <72869762+platobak@users.noreply.github.com>
2021-01-18 22:01:06 +0200

    with checkstyle

--- Statistics.java
+++ Statistics.java
@@ -2,7 +2,6 @@ package codemarathon;
 
 import java.awt.Color;
 import java.awt.Font;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -26,16 +25,16 @@ public class Statistics {
 
   public static double totalCovid() {
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases != null) {
-        count1 = count1 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases != null) {
+          count1 = count1 + 1;
+        }
       }
-    }
     }
 
    
-      double posostocovid = (count1 *100 /50);
-      return posostocovid;
+    double posostocovid = (count1 * 100 / 50);
+    return posostocovid;
   }
 
   /**
@@ -47,15 +46,15 @@ public class Statistics {
   public static double bus1Covid() {
     int count2 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases[i].getTransportation().equals("ΛΕΩΦΟΡΕΙΟ1"))  {
-        count2 = count2 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases[i].getTransportation().equals("ΛΕΩΦΟΡΕΙΟ1"))  {
+          count2 = count2 + 1;
+        }
       }
     }
-    }
     
-      double posostobus1 = (count2 *100 /50);
-      return posostobus1;
+    double posostobus1 = (count2 * 100 / 50);
+    return posostobus1;
   }
 
   /**
@@ -68,15 +67,15 @@ public class Statistics {
   public static double bus2Covid() {
     int count3 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases[i].getTransportation().equals("ΛΕΩΦΟΡΕΙΟ2")) {
-        count3 = count3 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases[i].getTransportation().equals("ΛΕΩΦΟΡΕΙΟ2")) {
+          count3 = count3 + 1;
+        }
       }
     }
-    }
    
-      double posostobus2 = (count3 * 100 /50);
-      return posostobus2;
+    double posostobus2 = (count3 * 100 / 50);
+    return posostobus2;
   }
 
   /**
@@ -89,15 +88,15 @@ public class Statistics {
   public static double atomikomesoCovid() {
     int count4 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases[i].getTransportation().equals("ΑΤΟΜΙΚΟΜΕΣΟ")) {
-        count4 = count4 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases[i].getTransportation().equals("ΑΤΟΜΙΚΟΜΕΣΟ")) {
+          count4 = count4 + 1;
+        }
       }
     }
-    }
     
-      double posostoatomikomeso = (count4 * 100 /50);
-      return posostoatomikomeso;
+    double posostoatomikomeso = (count4 * 100 / 50);
+    return posostoatomikomeso;
   }
 
   /**
@@ -109,15 +108,15 @@ public class Statistics {
   public static double logistirio() {
     int count5 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
+      if (Employee.employees[i].getHad_covid() == true) {
         if (CovidCases.cases[i].getUnit().equals("ΛΟΓΙΣΤΗΡΙΟ")) {
           count5 = count5 + 1;
         }
       }
     }
    
-      double posostologistirio = (count5 *100 /50);
-      return posostologistirio;
+    double posostologistirio = (count5 * 100 / 50);
+    return posostologistirio;
   }
 
   /**
@@ -130,15 +129,15 @@ public class Statistics {
   public static double marketing() {
     int count6 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases[i].getUnit().equals("ΜΑΡΚΕΤΙΝΓΚ")) {
-        count6 = count6 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases[i].getUnit().equals("ΜΑΡΚΕΤΙΝΓΚ")) {
+          count6 = count6 + 1;
+        }
       }
     }
-    }
     
-      double posostomarketing = (count6 *100 /50);
-      return posostomarketing;
+    double posostomarketing = (count6 * 100 / 50);
+    return posostomarketing;
   }
 
   /**
@@ -151,15 +150,15 @@ public class Statistics {
   public static double it() {
     int count7 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases[i].getUnit().equals("IT")) {
-        count7 = count7 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases[i].getUnit().equals("IT")) {
+          count7 = count7 + 1;
+        }
       }
-    	}
     }
       
-        double posostoit = (count7 *100 /50);
-        return posostoit;
+    double posostoit = (count7 * 100 / 50);
+    return posostoit;
   }
 
   /**
@@ -172,15 +171,15 @@ public class Statistics {
   public static double humanResources() {
     int count8 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases[i].getUnit().equals("ΑΝΘΡΩΠΙΝΟ-ΔΥΝΑΜΙΚΟ")) {
-        count8 = count8 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases[i].getUnit().equals("ΑΝΘΡΩΠΙΝΟ-ΔΥΝΑΜΙΚΟ")) {
+          count8 = count8 + 1;
+        }
       }
     }
-    }
     
-      double posostohr = (count8 *100 /50);
-     return posostohr;
+    double posostohr = (count8 * 100 / 50);
+    return posostohr;
   }
 
   /**
@@ -193,15 +192,15 @@ public class Statistics {
   public static double cleaning() {
     int count9 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases[i].getUnit().equals("ΚΑΘΑΡΙΟΤΗΤΑ")) {
-        count9 = count9 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases[i].getUnit().equals("ΚΑΘΑΡΙΟΤΗΤΑ")) {
+          count9 = count9 + 1;
+        }
       }
     }
-    }
     
-      double posostocleaning = (count9 *100 /50);
-      return posostocleaning;
+    double posostocleaning = (count9 * 100 / 50);
+    return posostocleaning;
   }
 
   /**
@@ -214,14 +213,14 @@ public class Statistics {
   public static double sales() {
     int count10 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases[i].getUnit().equals("ΠΩΛΗΣΕΙΣ"))  {
-        count10 = count10 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases[i].getUnit().equals("ΠΩΛΗΣΕΙΣ"))  {
+          count10 = count10 + 1;
+        }
       }
     }
-    }
    
-      double posostosales = (count10 *100 /50);
+    double posostosales = (count10 * 100 / 50);
     return posostosales;
   }
 
@@ -235,15 +234,15 @@ public class Statistics {
   public static double dieuthunsh() {
     int count11 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases[i].getUnit().equals("ΔΙΕΥΘΥΝΣΗ")) {
-        count11 = count11 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases[i].getUnit().equals("ΔΙΕΥΘΥΝΣΗ")) {
+          count11 = count11 + 1;
+        }
       }
     }
-    }
    
-      double posostodieuthunsh = (count11 *100 /50);
-     return posostodieuthunsh;
+    double posostodieuthunsh = (count11 * 100 / 50);
+    return posostodieuthunsh;
   }
 
   /**
@@ -256,15 +255,15 @@ public class Statistics {
   public static double customerService() {
     int count12 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases[i].getUnit().equals("ΕΞΥΠΗΡΕΤΗΣΗ-ΠΕΛΑΤΩΝ")) {
-        count12 = count12 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases[i].getUnit().equals("ΕΞΥΠΗΡΕΤΗΣΗ-ΠΕΛΑΤΩΝ")) {
+          count12 = count12 + 1;
+        }
       }
     }
-    }
     
-      double posostocustomerservice = (count12 *100 /50);
-     return posostocustomerservice;
+    double posostocustomerservice = (count12 * 100 / 50);
+    return posostocustomerservice;
   }
 
   /**
@@ -277,15 +276,15 @@ public class Statistics {
   public static double pr() {
     int count13 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases[i].getUnit().equals("ΔΗΜΟΣΙΕΣ-ΣΧΕΣΕΙΣ")) {
-        count13 = count13 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases[i].getUnit().equals("ΔΗΜΟΣΙΕΣ-ΣΧΕΣΕΙΣ")) {
+          count13 = count13 + 1;
+        }
       }
     }
-    }
     
-      double posostopr = (count13 *100 /50);
-     return posostopr;
+    double posostopr = (count13 * 100 / 50);
+    return posostopr;
   }
 
   /**
@@ -298,15 +297,15 @@ public class Statistics {
   public static double edu() {
     int count14 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases[i].getUnit().equals("ΕΚΠΑΙΔΕΥΣΗ")) {
-        count14 = count14 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases[i].getUnit().equals("ΕΚΠΑΙΔΕΥΣΗ")) {
+          count14 = count14 + 1;
+        }
       }
     }
-    }
    
-      double posostoedu = (count14 *100 /50);
-     return posostoedu;
+    double posostoedu = (count14 * 100 / 50);
+    return posostoedu;
   }
 
   /**
@@ -319,15 +318,15 @@ public class Statistics {
   public static double women() {
     int count15 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases[i].getSex().equals("Α")) {
-        count15 = count15 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases[i].getSex().equals("Α")) {
+          count15 = count15 + 1;
+        }
       }
     }
-    }
     
-      double posostowomen = (count15 *100 /50);
-      return posostowomen;
+    double posostowomen = (count15 * 100 / 50);
+    return posostowomen;
   }
 
   /**
@@ -340,15 +339,15 @@ public class Statistics {
   public static double men() {
     int count16 = 0;
     for (int i = 0; i < CovidCases.cases.length; i++) {
-    	if (Employee.employees[i].getHad_covid() == true) {
-      if (CovidCases.cases[i].getSex().equals("Θ")) {
-        count16 = count16 + 1;
+      if (Employee.employees[i].getHad_covid() == true) {
+        if (CovidCases.cases[i].getSex().equals("Θ")) {
+          count16 = count16 + 1;
+        }
       }
     }
-    }
    
-      double posostomen = (count16 *100 /50);
-      return posostomen;
+    double posostomen = (count16 * 100 / 50);
+    return posostomen;
   }
   /**
    * Η μέθοδος αυτή χρησιμοποιείται για τον υπολογισμό των ασθενών
@@ -358,42 +357,42 @@ public class Statistics {
   public static void symptoms_hospital() {
     int countsymptoms = 0;
     int counthospital = 0;
-    int countp = 0 ;
-      for (int x = 0; x < CovidCases.cases.length; x++) {
-    	  if (Employee.employees[x].getHad_covid() == true) {
-    		countp++;
-          if (CaseTreatment.casetreatments[x].getDaysymptoms() > 0) {
-            countsymptoms += 1;
-          }
-          if (CaseTreatment.casetreatments[x].getDayhospital() > 0) {
-            counthospital += 1;
-          }
+    int countp = 0;
+    for (int x = 0; x < CovidCases.cases.length; x++) {
+      if (Employee.employees[x].getHad_covid() == true) {
+        countp++;
+        if (CaseTreatment.casetreatments[x].getDaysymptoms() > 0) {
+          countsymptoms += 1;
+        }
+        if (CaseTreatment.casetreatments[x].getDayhospital() > 0) {
+          counthospital += 1;
+        }
 
-    	  }
       }
-      JPanel panel = new JPanel();
-      panel.setLayout(null);
-      panel.setBackground(new Color(203, 239, 240));
-      JFrame frame = new JFrame(); 
-      frame.add(panel);
-      frame.setTitle(""); 
-      frame.setResizable(false); 
-      frame.setSize(850, 150);
-      frame.setLocationRelativeTo(null);
-      frame.setVisible(true); 
-      frame.getContentPane().setBackground(new Color(203, 239, 240)); 
-      JLabel label1 = new JLabel(); 
-      label1.setText(">> The percentage of covid cases with severe symptoms is: " + countsymptoms * 100 / countp + "% <<");
-      label1.setBounds(50, 30, 800, 25);
-      label1.setFont(new Font("", Font.BOLD, 15));
-      panel.add(label1);
-      JLabel label2 = new JLabel();
-     label2.setText(">> The percentage of covid cases who stayed at least
-		    + "one day at the hospital is : "+ counthospital * 100 / countp +"% <<");
-      label2.setBounds(50, 50, 800, 25);
-      label2.setFont(new Font("", Font.BOLD, 15));
-      panel.add(label2);
-      
+    }
+    JPanel panel = new JPanel();
+    panel.setLayout(null);
+    panel.setBackground(new Color(203, 239, 240));
+    JFrame frame = new JFrame(); 
+    frame.add(panel);
+    frame.setTitle(""); 
+    frame.setResizable(false); 
+    frame.setSize(650, 150);
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true); 
+    frame.getContentPane().setBackground(new Color(203, 239, 240)); 
+    JLabel label1 = new JLabel(); 
+    label1.setText("The persentage of Covid cases with severe symptoms is: " 
+                                      + countsymptoms * 100 / countp + "%");
+    label1.setBounds(50, 30, 800, 25);
+    label1.setFont(new Font("", Font.BOLD, 15));
+    panel.add(label1);
+    JLabel label2 = new JLabel();
+    label2.setText("The percentage of Covid cases hospitalized is: "
+                                            + counthospital * 100 / countp + "%");
+    label2.setBounds(50, 50, 800, 25);
+    label2.setFont(new Font("", Font.BOLD, 15));
+    panel.add(label2);
        
   }
 }
