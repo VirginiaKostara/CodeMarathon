7743a4cbfc47e239e38c92df1766cb452ac5fa86
Virginia Kostara <72817607+VirginiaKostara@users.noreply.github.com>
2021-01-18 21:14:22 +0200

    final

--- CaseTreatment.java
+++ CaseTreatment.java
@@ -46,7 +46,7 @@ public class CaseTreatment implements Runnable {
   
   public static void initializeCaseTreatment() {
     for (int i = 0; i < 50; i++) {
-      new CaseTreatment(0, "τίποτα", false, i + 1);
+      new CaseTreatment(0, "none", false, i + 1);
     }
   }
 
@@ -101,22 +101,22 @@ public class CaseTreatment implements Runnable {
     String x;
     double fev = 0;
     try {
-      System.out.println("Εισάγετε το είδος των συμπτωμάτων"
-              + " του ασθενούς (καθόλου/ήπια/σοβαρά)");
+      System.out.println("Insert the type of symptoms that"
+              + " the patient has (none/mild/severe):");
       synchronized (input2) {
         do {
           abc = input2.nextLine();
           setSymptoms(abc);
-          if (!(abc.equals("καθόλου")) && !(abc.equals("ΚΑΘΟΛΟΥ"))
-                 && !(abc.equals("ΗΠΙΑ")) && !(abc.equals("ήπια"))
-                 && !(abc.equals("σοβαρά")) && !(abc.equals("ΣΟΒΑΡΑ"))) {
-            System.out.println("Λάθος εισαγωγή είδους συμπτωμάτων. Ξαναπροσπαθήστε");
+          if (!(abc.equals("none")) && !(abc.equals("NONE"))
+                 && !(abc.equals("MILD")) && !(abc.equals("mild"))
+                 && !(abc.equals("severe")) && !(abc.equals("SEVERE"))) {
+            System.out.println("Wrong insertion. Please retry.");
           }
-        } while (!(abc.equals("καθόλου")) && !(abc.equals("ΚΑΘΟΛΟΥ"))
-              && !(abc.equals("ΗΠΙΑ")) && !(abc.equals("ήπια"))
-              && !(abc.equals("σοβαρά")) && !(abc.equals("ΣΟΒΑΡΑ")));
+        } while (!(abc.equals("none")) && !(abc.equals("NONE"))
+              && !(abc.equals("MILD")) && !(abc.equals("mild"))
+              && !(abc.equals("severe")) && !(abc.equals("SEVERE")));
         Thread.sleep(100);
-        System.out.println("Εισάγετε τη θερμοκρασία του ασθενούς");
+        System.out.println("Insert fever:");
         boolean f3 = true;
         do {
           f3 = false;
@@ -127,29 +127,29 @@ public class CaseTreatment implements Runnable {
               input2.nextLine();
             } else {
               f3 = true;
-              System.out.println("Παρακαλώ εισάγετε έναν"
-                            + " αριθμό που να αντιστοιχεί σε θερμοκρασία "
-                            + "ανθρώπινου σώματος");
+              System.out.println("Please insert a number"
+                            + " which correspond with the temperature of"
+                            + "a human being.");
             }
           } catch (InputMismatchException e) {
-            System.out.println("Παρακαλώ εισάγετε έναν ακέραιο ή δεκαδικό αριθμό");
+            System.out.println("Please insert a integer or a decimal number.");
             input2.nextLine();
             f3 = true;
           }
         } while (f3 == true);
-        System.out.println("Είναι ο ασθενής στο νοσοκομείο; (ΝΑΙ/ΟΧΙ)");
+        System.out.println("Is the patient at the hospital (YES/NO)");
         do {
           x = input2.nextLine();
-          if (!(x.equals("ΝΑΙ")) && !(x.contentEquals("ναι")) && !(x.contentEquals("όχι"))
-                        && !(x.contentEquals("ΟΧΙ"))) {
-            System.out.println("Λάθος εισαγωγή δεδομένων"
-                          + " αναφορικά με την είσοδο του ασθενούς "
-                            + "στο νοσοκομείο. Παρακαλώ ξαναπροσπαθήστε");
+          if (!(x.equals("YES")) && !(x.contentEquals("yes")) && !(x.contentEquals("no"))
+                        && !(x.contentEquals("NO"))) {
+            System.out.println("Wrong data input"
+                          + " regarding the patient's stay at the "
+                            + "hospital. Please retry.");
           }
-        } while (!(x.equals("ΝΑΙ")) && !(x.contentEquals("ναι")) && !(x.contentEquals("όχι"))
-                      && !(x.contentEquals("ΟΧΙ")));
+        } while (!(x.equals("YES")) && !(x.contentEquals("yes")) && !(x.contentEquals("no"))
+                      && !(x.contentEquals("NO")));
 
-        if (x.contentEquals("ΝΑΙ") || x.contentEquals("ναι")) {
+        if (x.contentEquals("YES") || x.contentEquals("yes")) {
           setHospital(true);
           
           if (getCode() >= 0 && getCode() < 50) { // neo
@@ -160,20 +160,20 @@ public class CaseTreatment implements Runnable {
         } else {
           setHospital(false);
         }
-        System.out.println("Επιτυχής καταχώρηση");
+        System.out.println("Successful Insertion");
         System.out.println();
         if (getCode() >= 0 && getCode() < 50) { // neo
-            if (getSymptoms().equals("ΣΟΒΑΡΑ") || getSymptoms().equals("σοβαρά")) {
+            if (getSymptoms().equals("SEVERE") || getSymptoms().equals("severe")) {
           setDaysymptoms(getDaysymptoms() + 1);
             }
-        } // neo
+        }
       }
         
       
     } catch (InterruptedException e) {
-      System.err.println("Κάτι προέκυψε. Παρακαλώ προσπαθήστε αργότερα.");
+      System.err.println("Something happened. Please retry");
     } finally {
-      System.out.println("Η εισαγωγή δεδομένων τελείωσε!");
+      System.out.println("The insertion is finished. Thank you!");
       System.out.println();
     }
 
@@ -197,10 +197,10 @@ public class CaseTreatment implements Runnable {
     int code2;
     if (Z < 3) {
       System.out.println();
-      CovidCases.printCasesnow(); // neo
+      CovidCases.printCasesnow(); 
       System.out.println(
-            "Παρακαλώ γράψτε τον κωδικό του εργαζομένου για τον οποίο"
-             + " ενημερωθήκατε για την κατάσταση της υγείας του "); // neo
+            "Please write the employee's id that you want to save"
+             + " his/her today status. "); 
       boolean f1 = true;
       while (f1 == true) {
         try {
@@ -214,7 +214,7 @@ public class CaseTreatment implements Runnable {
               }
             }
             if (frouros == false) {
-              System.out.println("Λάθος εισαγωγή id. Ξαναπροσπαθήστε.");
+              System.out.println("Wrong id. Please retry.");
             }
           } while (frouros == false);
           covidcase[0] = new CaseTreatment(36.6, "", false, code1);
@@ -225,8 +225,8 @@ public class CaseTreatment implements Runnable {
           thread[0].start(); // δεν μπορει να χειριστει την περιπτωση που
         //υπάρχουν 2 ενεργα κρουσματα στην εταιρεία
         } catch (InputMismatchException e) {
-          System.out.println("To id που εισάγατε δεν αντιστοιχεί"
-                    + " σε εργαζόμενο. Παρακαλώ ξαναπροσπαθήστε");
+          System.out.println("The given id does not belong to any"
+                    + " employee. Please retry");
           input.nextLine();
           f1 = true;
         }
@@ -240,8 +240,8 @@ public class CaseTreatment implements Runnable {
         }
         CovidCases.printCases();
         System.out.println(
-            "Παρακαλώ γράψτε τον κωδικό του εργαζομένου για τον οποίο"
-            + " ενημερωθήκατε για την κατάσταση της υγείας του "); // neo
+            "Please write the employee's id that you want to save"
+             + " his/her today status. "); 
         boolean f2 = true;
         while (f2 == true) {
           try {
@@ -255,15 +255,15 @@ public class CaseTreatment implements Runnable {
                 }
               }
               if (frouros2 == false) {
-                System.out.println("Λάθος εισαγωγή id. Ξαναπροσπαθήστε.");
+                System.out.println("Wrong id. Please retry.");
               }
             } while (frouros2 == false);
             covidcase[1] = new CaseTreatment(36.6, "", false, code2);
             thread[1] = new Thread(covidcase[1]);
             thread[1].start();
           } catch (InputMismatchException e) {
-            System.out.println("To id που εισάγατε δεν αντιστοιχεί"
-                      + " σε εργαζόμενο. Παρακαλώ ξαναπροσπαθήστε");
+            System.out.println("The given id does not belong to any"
+                    + " employee. Please retry");
             input.nextLine();
             f2 = true;
           }
